33a6738458a518b121efa2648bc5ffb75ba47e6a
Varun kumar <pro@MACs-MacBook-Pro.local>
2022-11-14 23:41:44 +0530

    Felt happy in fixing mongoose db error

--- server.js
+++ server.js
@@ -5,23 +5,49 @@ const mongoose = require('mongoose')
 const User = require('./model/user')
 const bcrypt = require('bcryptjs')
 
-mongoose.connect('mongodb://localhost:27017/login-app-db')
-
+mongoose.connect('mongodb://127.0.0.1:27017/login-app-db').then(()=> {console.log('Mongoose connected')}).catch(err => console.log(err))
+//ds
 const app = express()
 app.use('/', express.static(path.join(__dirname,'static')))
 app.use(bodyParser.json())
 
-app.post('/api/register', (req, res) => {
+app.post('/api/register', async (req, res) => {
     
-    console.log(req.body)
+    //console.log('Log message of /api/register:', req.body)
     const {username, password: plainTextPassword} = req.body
-    
-    const password =  bcrypt.hash(password, 10)
-    //console.log(bcrypt.hash(password, 10))
+    if(!username || typeof username !== 'string')
+    {
+        return res.json({status: 'error', error: 'Invalid username datatype'})
+    }
+    if(!plainTextPassword || typeof plainTextPassword !== 'string')
+    {
+        return res.json({status: 'error', error: 'Invalid password datatype'})
+    }
+    if(plainTextPassword.length < 5)
+    {
+        return res.json({status: 'error', error: 'Invalid Password length'})
+    }
+    const password =  await bcrypt.hash(plainTextPassword, 10)
+    try{
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created succesfully:', response)
+    }catch(error){
+        if(error.code === 11000)
+        {
+            return res.json({status: 'error', error: 'Duplicate key'})
+        }
+        throw error
+        //console.log(JSON.stringify(error))
+       // return res.json({status: 'error'})
+    }
+
     res.json({ status : 'ok' })
 })
 
-app.listen(9996, ()=> {
+app.listen(9999, ()=> {
     console.log('Server up at 9999')
 })
 
